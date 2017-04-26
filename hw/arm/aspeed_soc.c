465a3f5bd7df6bcf8e3d5de0b6300cb14d3f8131
Joel Stanley <joel@jms.id.au>
2017-04-26 22:40:01 +0930

    aspeed: Add DPS310 temperature sensor
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- hw/arm/aspeed_soc.c
+++ hw/arm/aspeed_soc.c
@@ -274,6 +274,10 @@ static void aspeed_soc_realize(DeviceState *dev, Error **errp)
     object_property_set_int(OBJECT(dev), 20000, "temperature2", &err);
     object_property_set_int(OBJECT(dev), 110000, "temperature3", &err);
 
+    /* Add a DPS310 temperature/humidity sensor */
+    dev = i2c_create_slave(aspeed_i2c_get_bus(DEVICE(&s->i2c), 2),
+                           "dps310", 0x76);
+
     /* The palmetto platform expects a ds3231 RTC but a ds1338 is
      * enough to provide basic RTC features. Alarms will be missing */
     i2c_create_slave(aspeed_i2c_get_bus(DEVICE(&s->i2c), 0), "ds1338", 0x68);
