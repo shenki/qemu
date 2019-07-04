c6aba828d12b49474c61984022d6c3920340683e
Cédric Le Goater <clg@kaod.org>
2019-07-04 08:05:02 +0200

    ast2400: add a iBT device model
    Implement an IPMI BT interface model using a chardev backend to
    communicate with an external PowerNV guest. The Protocol is the
    OpenIPMI simulator protocol for a virtual machine described in :
    
        https://github.com/cminyard/openipmi/blob/master/lanserv/README.vm
    
    and implemented by the 'ipmi-bmc-extern' model on the host side.
    
    To use, start the BMC guest with :
    
        -chardev socket,id=ipmi0,host=localhost,port=9002,ipv4,server,nowait \
        -global driver=aspeed.ibt,property=chardev,value=ipmi0
    
    and the PowerNV guest with :
    
        -chardev socket,id=ipmi0,host=localhost,port=9002,reconnect=10 \
        -device ipmi-bmc-extern,id=bmc0,chardev=ipmi0 \
        -device isa-ipmi-bt,bmc=bmc0,irq=10
    
    This is work in progress. Use with care and kindness, it's probably
    quite buggy.
    
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

--- include/hw/arm/aspeed_soc.h
+++ include/hw/arm/aspeed_soc.h
@@ -25,6 +25,7 @@
 #include "hw/net/ftgmac100.h"
 #include "hw/gpio/aspeed_gpio.h"
 #include "hw/sd/aspeed_sdhci.h"
+#include "hw/misc/aspeed_ibt.h"
 
 #define ASPEED_SPIS_NUM  2
 #define ASPEED_WDTS_NUM  3
@@ -53,6 +54,7 @@ typedef struct AspeedSoCState {
     FTGMAC100State ftgmac100[ASPEED_MACS_NUM];
     AspeedGPIOState gpio;
     AspeedSDHCIState sdhci;
+    AspeedIBTState ibt;
 } AspeedSoCState;
 
 #define TYPE_ASPEED_SOC "aspeed-soc"
